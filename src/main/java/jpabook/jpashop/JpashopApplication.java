196ed7e5eafef241ae62ac7f15247afeff9ecdee
Taksu <53938366+leegyeongwhan@users.noreply.github.com>
2022-07-12 11:59:43 +0900

    웹 계층 개발 홈화면,레이아웃,회원 등록 화면

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
     public static void main(String[] args) {
-        Hello hello = new Hello();
-        hello.setData("hello");
-        String str = hello.getData();
-        System.out.println(str);
+
         SpringApplication.run(JpashopApplication.class, args);
     }
 
