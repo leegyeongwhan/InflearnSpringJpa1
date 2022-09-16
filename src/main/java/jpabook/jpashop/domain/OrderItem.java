940e7d8c76fcbf25ac3f379017bff3b47bf480d2
leegyeongwhan <leekhy02@naver.com>
2022-09-16 21:35:59 +0900

    feat: Order OrderItem 생성 메서드, 비지니스 로직 구현

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,16 +4,22 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static javax.persistence.FetchType.*;
 
 @Entity
-@Getter@Setter
+@Getter
+@Setter
 public class OrderItem {
 
-    @Id@GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_item_id")
     public Long id;
 
@@ -27,4 +33,30 @@ public class OrderItem {
 
     private int orderPrice; //주문가격
     private int count;     //주문 수량
+
+    //== 생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //== 비지니스로직 ==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //== 조회 로직 ==//
+
+    /**
+     * 주문 상품 전체가격조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
