a2285babadcd62628f21ae4227a10affe3e2522c
leegyeongwhan <leekhy02@naver.com>
2022-09-22 19:47:10 +0900

    feat: 상품 등록 폼 켐플릿 개발

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -3,16 +3,16 @@ package jpabook.jpashop.controller;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter@Setter
 public class BookForm {
 
     private Long id;
-    private int price;
-    private int stockQuantity;
 
+    private String name;
+    private int price;
+
+    private int stockQuantity;
     private String author;
     private String isbn;
-    private String name;
 }
--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -12,25 +12,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
-
     private final ItemService itemService;
-
-    @GetMapping("item/new")
-    public String creatForm(Model model) {
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
-        return "item/createForm";
+        return "items/createItemForm";
     }
-
-    @PostMapping("item/new")
+    @PostMapping(value = "/items/new")
     public String create(BookForm form) {
         Book book = new Book();
+
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
-        book.setIsbn(form.getAuthor());
         book.setAuthor(form.getAuthor());
-
+        book.setIsbn(form.getIsbn());
         itemService.saveItem(book);
-        return "redirect:/";
+        return "redirect:/items";
     }
 }
